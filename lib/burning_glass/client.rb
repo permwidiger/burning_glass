354f40a5edcabf011ac58500a06a4be0d3c351ae
Matt Widiger <mwidiger@percipiomedia.com>
2014-11-17 13:35:41 -0500

    Added parse_from_file

--- lib/burning_glass/client.rb
+++ lib/burning_glass/client.rb
@@ -12,16 +12,23 @@ module Burning_Glass
     def initialize(options={})
       @wsdl = options[:wsdl]
       @instanceCode = options[:instanceCode] || 'YOUR_BURNING_GLASS_INSTANCE_CODE_CAN_GO_HERE'
-      @svcCall = options[:svcCall] || 'tag_binary_data'
-      @svcResponse = options[:svcResponse] ||  'tag_binary_data_response'
-      @svcReturn = options[:svcReturn] || 'tag_binary_data_return'
+      @svcCall = options[:svcCall] || :tag_binary_data
+      @svcResponse = options[:svcResponse] ||  :tag_binary_data_response
+      @svcReturn = options[:svcReturn] || :tag_binary_data_return
     end
 
     def connection
       Savon.client(wsdl: wsdl, log: false)
     end
 
-    def parse(file)
+    def parse(file, file_extension)
+      params = {
+        instance_code: @instanceCode,
+        binary_data: Base64.encode64(file),
+        ext: file_extension,
+        transactionID: SecureRandom.random_number(36**64).to_s(36).rjust(64, "0")
+      }
+
       result = connection.call(@svcCall, message: {InstanceCode: @instanceCode, BinaryData: Base64.encode64(file)})
       Resume.parse(result.body[@svcResponse][@svcReturn])
     end
